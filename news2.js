74210b39fdff1bcaf78ff3187f7c458810f8eb52
derekgouuui2-svg <derekgouuui2@gmail.com>
2026-05-01 08:14:07 -0500

    Add files via upload

---
+++ news2.js
@@ -0,0 +1,14 @@
+
+function navbar() {
+	
+	var nav = document.getElementById("navbar");
+	if (nav.style.width === "0px" || nav.style.width === ""){
+		nav.style.width = "100px";
+		nav.style.display = "block";
+	}
+	else {
+		nav.style.width = "0px"
+		nav.style.display = "none";
+	}
+	
+}
